eb588de96226986244810f5845f13337f23d1f37
Gplus808 <gplus808@gmail.com>
2024-02-07 10:05:50 +0000

    error resolved

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './ArticleList.css'
-import { getArticles } from "./api";
+import { getArticles } from "./Api";
 import ArticleList from "./ArticleList";
 
 
