f05fe61a830040da563f233c35b564db99fc696b
Gplus808 <gplus808@gmail.com>
2024-02-09 15:13:23 +0000

    getting ready for hosting

--- src/Components/ArticleById.jsx
+++ src/Components/ArticleById.jsx
@@ -5,12 +5,14 @@ import { getArticleById, getComments } from './Api';
 import moment from 'moment';
 import Comments from './Comments';
 import Votes from './Votes';
+import PostCommentForm from './PostCommentForm';
 
 function ArticleById() {
     const { articleId } = useParams();
     const [article, setArticle] = useState(null);
     const [showComments, setShowComments] = useState(false);
     const [error, setError] = useState(null);
+    const [showPostCommentForm, setShowPostCommentForm] = useState(false);
   
     useEffect(() => {
         getArticleById(articleId)
@@ -22,9 +24,13 @@ function ArticleById() {
             setError(`Failed to load articles: ${error}`);
           });
       }, [articleId]);
-
-
-    const toggleComments = () => setShowComments(!showComments);
+      
+    const toggleComments = () => {
+      setShowComments(!showComments)
+    };
+    const handleCommentPosted = () => {
+      setShowComments(true)
+    }
 
   
     if (error) return <p>Error loading article: {error}</p>;
@@ -43,8 +49,10 @@ function ArticleById() {
         <p>Published at: {dateTime.format('DD-MM-YYYY HH:mm:ss')}</p>
         <Votes initialVotes={article.article.votes} articleId={articleId} />
         <p>{article.article.body}</p>
+        <button onClick={() => setShowPostCommentForm(!showPostCommentForm)}>Post a comment</button>
+        {showPostCommentForm && <PostCommentForm articleId={articleId} />}
         <button onClick={toggleComments}>View Comments</button>
-                {showComments && <Comments articleId={article.article.article_id} />}
+        {showComments && <Comments articleId={article.article.article_id} />}
         </div>
       </div>
     );
