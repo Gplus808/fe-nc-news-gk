45e6f613d9bf0b7838af04e0356a1d3b050ec562
Gplus808 <gplus808@gmail.com>
2024-04-11 19:09:54 +0100

    lazy loading implemented for images

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './ArticleList.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import moment from 'moment';
 
 
@@ -10,7 +12,7 @@ export default function ArticleList({articles}) {
     <section className="article-container">
     {articles.map((article) => (
       <div key={article.article_id} className="article-item">
-        <img className="article-image" src={article.article_img_url} />
+        <LazyLoadImage className="article-image" alt={article.title} src={article.article_img_url} effect="blur" />
         <div className="article-content">
         <Link to={`/articles/${article.article_id}`}>
             <button>View Article</button>
@@ -18,7 +20,7 @@ export default function ArticleList({articles}) {
         <p>Article ID: {article.article_id}</p>
         <p>Title: {article.title}</p>
         <p>Author: {article.author}</p>
-        {/* <p>Published at: {moment(article.created_at).format('DD-MM-YYYY HH:mm:ss')}</p> */}
+        <p>Published at: {moment(article.created_at).format('DD-MM-YYYY HH:mm:ss')}</p>
         <p>Like: {article.votes}</p>
         </div>
         </div>
